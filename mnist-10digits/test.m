bdcafb483f204b030ad705f8ac244343ed139eb1
tungedng2710 <tungnguyen99.tn@gmail.com>
2022-01-10 22:56:02 +0700

    update mnist test

--- mnist-10digits/test.m
+++ mnist-10digits/test.m
@@ -1,4 +1,4 @@
-clc; close all;
+clc; close all; clear;
 num_classes = 10;
 
 [XTrain,YTrain,anglesTrain] = digitTrain4DArrayData;
@@ -10,14 +10,32 @@ I = XTest;
 dlI = dlarray(I, 'SSCB');
 
 load ('model.mat', 'model');
-plot(model);
+[labels,scores] = classify(model,XTest);
+labels = double(labels);
 
 % Test Feature Extraction
 backbone = removeLayers(layerGraph(model),'output');
 backbone = removeLayers(backbone,'softmax');
 % backbone = removeLayers(backbone,'fc');
 % backbone = removeLayers(backbone,'relu_3');
-plot(backbone);
+% plot(backbone);
+
 backbone = dlnetwork(backbone);
-output = forward(backbone,dlI);
+
+feat_vecs = forward(backbone,dlI);
+feat_vecs = double(extractdata(feat_vecs));
+
+rng default % for reproducibility
+feat_vecs = tsne(feat_vecs','Algorithm','barneshut','NumPCAComponents',2);
+
+data = [feat_vecs, labels];
+
+cmap = colormap(parula(10));
+fh = figure;
+ah = axes(fh);
+hold(ah,'on');
+for i=1:5000
+    k = data(i,3);
+    plot(ah, data(i,1),data(i,2), '.', 'Color',cmap(k,:));
+end
 
